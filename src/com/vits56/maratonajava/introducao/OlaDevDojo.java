855ce7dd5dae64063b62df56b0dc63251ba0a691
vits56 <bastosduartebruno@gmail.com>
2022-09-01 11:33:25 -0300

    comentários

--- src/com/vits56/maratonajava/introducao/OlaDevDojo.java
+++ src/com/vits56/maratonajava/introducao/OlaDevDojo.java
@@ -1,6 +1,15 @@
 package com.vits56.maratonajava.introducao;
 
+/**
+ * @author vits56 comentário javadoc
+ */
 public class OlaDevDojo {
+    // comentario de uma linha
+
+    /*
+       comentario de multiplas linhas
+    */
+
     public static void main(String[] args) {
         System.out.println("Ola Dev Dojo");
     }
