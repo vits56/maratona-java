03c067a20e666af1f8f9e2f4a0713b964656a2de
vits56 <bastosduartebruno@gmail.com>
2022-09-02 11:53:55 -0300

    tipos primitivos parte 3 - casting

--- src/com/vits56/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/com/vits56/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,10 +3,10 @@ package com.vits56.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
-        int idade = 10;
+        int idade = (int) 10000000000L;
         long numeroGrande = 1000000000000000000L;
-        double salarioDouble = 2000;
-        float salarioFloat = 2500;
+        double salarioDouble = 2000.0D;
+        float salarioFloat = 2500.F;
         byte idadeByte = 20;
         short idadeShort = 21;
         boolean verdadeiro = true;
@@ -16,5 +16,6 @@ public class Aula02TiposPrimitivos {
         System.out.println("A idade é " + idade + " anos");
         System.out.println(falso);
         System.out.println("char " + caractere);
+
     }
 }
