1f2ed7e29054de79a43e2dbb9fad9b9ac3c925d8
vits56 <bastosduartebruno@gmail.com>
2022-09-04 19:58:02 -0300

    16 - Operadores pt 02 - Relacionais

--- src/com/vits56/maratonajava/introducao/Aula04Operadores.java
+++ src/com/vits56/maratonajava/introducao/Aula04Operadores.java
@@ -7,5 +7,22 @@ public class Aula04Operadores {
         double numero02 = 20;
         double resultado = numero01 * numero02;
         System.out.println(resultado);
+
+        // %
+
+        int resto = 20 % 2;
+        System.out.println(resto);
+
+        // < > <= >= == !=
+
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezDiferenteVinte = 10 != 20;
+
+        System.out.println("isDezMaiorQueVinte " + isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte " + isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte " + isDezIgualVinte);
+        System.out.println("isDezDiferenteVinte " + isDezDiferenteVinte);
     }
 }
