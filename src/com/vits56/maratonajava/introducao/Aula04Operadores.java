8ef96fb2c24ad01fcb954c3d16ff9678d5c30375
vits56 <bastosduartebruno@gmail.com>
2022-09-04 19:42:03 -0300

    15 - Operadores pt 01 - Aritméticos

---
+++ src/com/vits56/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,11 @@
+package com.vits56.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // + - / *
+        double numero01 = 10;
+        double numero02 = 20;
+        double resultado = numero01 * numero02;
+        System.out.println(resultado);
+    }
+}
