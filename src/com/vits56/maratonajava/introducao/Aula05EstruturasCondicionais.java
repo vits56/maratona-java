a87dab05c3d6097c876795219774e7c3c62695b5
vits56 <bastosduartebruno@gmail.com>
2022-09-05 21:39:55 -0300

    20 - Estruturas Condicionais pt 01 - IF

---
+++ src/com/vits56/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,12 @@
+package com.vits56.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+
+        int idade = 20;
+
+        if (idade >= 18) {
+            System.out.println("Autorizado a comprar bebida alcolica");
+        }
+    }
+}
