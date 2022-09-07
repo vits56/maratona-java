ea6f64ab1a1d5ff4a008af9bb5865142697ab7a1
vits56 <bastosduartebruno@gmail.com>
2022-09-07 20:16:34 -0300

    24 - Estruturas Condicionais pt 05 - Resolução do exercícios

--- src/com/vits56/maratonajava/introducao/Aula05EstruturasCondicionais04.java
+++ src/com/vits56/maratonajava/introducao/Aula05EstruturasCondicionais04.java
@@ -2,6 +2,25 @@ package com.vits56.maratonajava.introducao;
 
 public class Aula05EstruturasCondicionais04 {
     public static void main(String[] args) {
+        // 0 - 34.712 9.70%
+        // 34.713 - 68.507 37.35
+        // 68.508 >      49.50%
+
+        double bruto = 100000;
+        double tax1 = 9.7 / 100;
+        double tax2 = 37.35 / 100;
+        double tax3 = 49.50 / 100;
+        double valorImposto;
+
+        if (bruto <= 34712) {
+           valorImposto = tax1;
+        } else if (bruto >= 34713 && bruto <= 68507) {
+            valorImposto = tax2;
+        } else {
+            valorImposto = tax3;
+        }
+
+        System.out.println("Você terá que pagar de imposto: " + bruto * valorImposto +", E seu salário depois do desconto ficou em " + (bruto - (bruto * valorImposto)));
 
     }
 }
