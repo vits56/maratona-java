fbdba1b8ca4e0d87a88e149b310974177bdd59f2
vits56 <bastosduartebruno@gmail.com>
2022-09-11 22:56:34 -0300

    33 - Arrays pt 02

---
+++ src/com/vits56/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,15 @@
+package com.vits56.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        // byte, short, int, long, float, double = 0
+        // char '\u0000' ' '
+        // boolean false
+        // String null
+
+        char [] idades = new char[3];
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
