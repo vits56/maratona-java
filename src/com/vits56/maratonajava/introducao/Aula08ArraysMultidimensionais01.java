c92f7d520eb084a4a610b13e40e0ce95b05259a6
vits56 <bastosduartebruno@gmail.com>
2022-09-13 11:53:30 -0300

    37 - Arrays Multidimensionais pt 02 - Foreach

--- src/com/vits56/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
+++ src/com/vits56/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -2,7 +2,7 @@ package com.vits56.maratonajava.introducao;
 
 public class Aula08ArraysMultidimensionais01 {
     public static void main(String[] args) {
-        int [][] dias = new int[3][3];
+        int[][] dias = new int[3][3];
         dias[0][0] = 30;
         dias[0][1] = 31;
         dias[0][2] = 29;
@@ -18,6 +18,14 @@ public class Aula08ArraysMultidimensionais01 {
         for (int i = 0; i < dias.length; i++) {
             for (int j = 0; j < dias[i].length; j++) {
                 System.out.println(dias[i][j]);
+            }
+        }
+
+        System.out.println("----------");
+
+        for (int[] ref : dias) {
+            for (int dia : ref) {
+                System.out.println(dia);
             }
         }
 
