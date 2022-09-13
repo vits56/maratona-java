6b15c058c829f6f0ac5b7425867e5eb90a18f2c2
vits56 <bastosduartebruno@gmail.com>
2022-09-13 11:30:09 -0300

    36 - Arrays Multidimensionais pt 01

---
+++ src/com/vits56/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -0,0 +1,25 @@
+package com.vits56.maratonajava.introducao;
+
+public class Aula08ArraysMultidimensionais01 {
+    public static void main(String[] args) {
+        int [][] dias = new int[3][3];
+        dias[0][0] = 30;
+        dias[0][1] = 31;
+        dias[0][2] = 29;
+
+        dias[1][0] = 31;
+        dias[1][1] = 30;
+        dias[1][2] = 31;
+
+        dias[2][0] = 30;
+        dias[2][1] = 31;
+        dias[2][2] = 29;
+
+        for (int i = 0; i < dias.length; i++) {
+            for (int j = 0; j < dias[i].length; j++) {
+                System.out.println(dias[i][j]);
+            }
+        }
+
+    }
+}
