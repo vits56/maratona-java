8514d35b667a87a34265c7cbba12ad29662a8dc4
vits56 <bastosduartebruno@gmail.com>
2022-09-14 16:41:21 -0300

    39 - Orientação Objetos pt 01 - Introdução classes pt 01

---
+++ src/com/vits56/maratonajava/javacore/introducaoclasses/dominio/Estudante.java
@@ -0,0 +1,7 @@
+package com.vits56.maratonajava.javacore.introducaoclasses.dominio;
+
+public class Estudante {
+    public String nome;
+    public int idade;
+    public char sexo;
+}
---
+++ src/com/vits56/maratonajava/javacore/introducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,16 @@
+package com.vits56.maratonajava.javacore.introducaoclasses.test;
+
+import com.vits56.maratonajava.javacore.introducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "João";
+        estudante.idade = 25;
+        estudante.sexo = 'M';
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+}
+
