2c5847f7650e3b2d980db5e7f6f5cfdfa8f6c0b6
vits56 <bastosduartebruno@gmail.com>
2022-09-15 14:26:04 -0300

    44 - Orientação Objetos - Métodos pt 01

---
+++ src/com/vits56/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -0,0 +1,14 @@
+package com.vits56.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Calculadora {
+
+    public void somaDoisNumeros() {
+        System.out.println(10 + 10);
+    }
+
+    public void subtraiDoisNumeros() {
+        System.out.println(10 - 10);
+    }
+
+
+}
---
+++ src/com/vits56/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest01.java
@@ -0,0 +1,11 @@
+package com.vits56.maratonajava.javacore.Bintroducaometodos.test;
+
+import com.vits56.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+        calculadora.subtraiDoisNumeros();
+    }
+}
