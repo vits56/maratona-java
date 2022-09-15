0247fddf2a2b07aa33b72afe683ec83dc5f323ba
vits56 <bastosduartebruno@gmail.com>
2022-09-15 11:26:50 -0300

    42 - Orientação Objetos - Exercício Classes

---
+++ src/com/vits56/maratonajava/javacore/Aintroducaoclasses/dominio/Carro.java
@@ -0,0 +1,7 @@
+package com.vits56.maratonajava.javacore.Aintroducaoclasses.dominio;
+
+public class Carro {
+    public String nome;
+    public String modelo;
+    public int ano;
+}
---
+++ src/com/vits56/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
@@ -0,0 +1,29 @@
+package com.vits56.maratonajava.javacore.Aintroducaoclasses.test;
+
+import com.vits56.maratonajava.javacore.Aintroducaoclasses.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        Carro carro2 = new Carro();
+        Carro carro3 = new Carro();
+
+
+        carro.nome = "Ferrari";
+        carro.modelo = "F40";
+        carro.ano = 1992;
+
+        carro2.nome = "Honda";
+        carro2.modelo = "Civic";
+        carro2.ano = 2003;
+
+        carro3.nome = "Fiat";
+        carro3.modelo = "Uno";
+        carro3.ano = 1998;
+
+        System.out.println("Nome: " + carro.nome + ", modelo: " + carro.modelo + ", ano: " + carro.ano);
+        System.out.println("Nome: " + carro2.nome + ", modelo: " + carro2.modelo + ", ano: " + carro2.ano);
+        System.out.println("Nome: " + carro3.nome + ", modelo: " + carro3.modelo + ", ano: " + carro3.ano);
+
+    }
+}
