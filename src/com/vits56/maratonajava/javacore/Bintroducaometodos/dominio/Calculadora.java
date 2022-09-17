613c3b2759903c36a90352848fc9a27b36753e87
vits56 <bastosduartebruno@gmail.com>
2022-09-17 11:01:31 -0300

    48 - Orientação Objetos - Métodos pt 05 - Parâmetros tipo primitivo

--- src/com/vits56/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/com/vits56/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -38,5 +38,13 @@ public class Calculadora {
         System.out.println(num1 / num2);
     }
 
+    public void alteraDoisNumeros(int num1, int num2) {
+        num1 = 99;
+        num2 = 33;
+        System.out.println("Dentro do altera dois números");
+        System.out.println("num1: " + num1);
+        System.out.println("num2: " + num2);
+    }
+
 
 }
