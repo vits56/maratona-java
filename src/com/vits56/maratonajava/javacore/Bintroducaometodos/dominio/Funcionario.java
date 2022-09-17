c445f26c27e1d1d3903f731de3fd9cae0d880e6c
vits56 <bastosduartebruno@gmail.com>
2022-09-17 19:05:46 -0300

    56 - Orientação Objetos - Modificador de acesso private, get e set pt 03

--- src/com/vits56/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
+++ src/com/vits56/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -1,9 +1,10 @@
 package com.vits56.maratonajava.javacore.Bintroducaometodos.dominio;
 
 public class Funcionario {
-    public String nome;
-    public int idade;
-    public double[] salarios;
+    private String nome;
+    private int idade;
+    private double[] salarios;
+    private double media;
 
     public void imprime() {
         System.out.println(this.nome);
@@ -18,11 +19,39 @@ public class Funcionario {
         if (this.salarios == null) {
             return;
         }
-        double media = 0;
+
         for (double salario : salarios) {
             media += salario;
         }
         media = media / salarios.length;
         System.out.println("Média salarial: " + media);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
     }
 }
--- src/com/vits56/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
+++ src/com/vits56/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
@@ -5,12 +5,12 @@ import com.vits56.maratonajava.javacore.Bintroducaometodos.dominio.Funcionario;
 public class FuncionarioTest01 {
     public static void main(String[] args) {
         Funcionario funcionario01 = new Funcionario();
-        funcionario01.nome = "Goku";
-        funcionario01.idade = 40;
-        funcionario01.salarios = new double[3];
-        funcionario01.salarios[0] = 1000;
-        funcionario01.salarios[1] = 2000;
-        funcionario01.salarios[2] = 3000;
+        funcionario01.setNome("Goku");
+        funcionario01.setIdade(40);
+        funcionario01.setSalarios(new double[3]);
+        funcionario01.getSalarios()[0] = 1000;
+        funcionario01.getSalarios()[1] = 2000;
+        funcionario01.getSalarios()[2] = 3000;
 
         funcionario01.imprime();
     }
