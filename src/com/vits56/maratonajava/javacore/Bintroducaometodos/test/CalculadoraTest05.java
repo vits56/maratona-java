51eec4d5f7a7f5f91e2ea83987f0e77ae7e9cf80
vits56 <bastosduartebruno@gmail.com>
2022-09-17 11:56:48 -0300

    52 - Orientação Objetos - Métodos pt 09 - Varargs

---
+++ src/com/vits56/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest05.java
@@ -0,0 +1,13 @@
+package com.vits56.maratonajava.javacore.Bintroducaometodos.test;
+
+import com.vits56.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest05 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        int[] numeros = {1, 2, 3, 4, 5};
+
+        calculadora.somaArray(numeros);
+        calculadora.somaVarArgs(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    }
+}
