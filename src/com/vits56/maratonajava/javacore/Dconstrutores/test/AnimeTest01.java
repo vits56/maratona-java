22da553f72f310f5b174c37c5173ba01c4128615
vits56 <bastosduartebruno@gmail.com>
2022-09-18 13:57:18 -0300

    59 - Orientação Objetos - Construtores pt 02 - Sobrecarga

--- src/com/vits56/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/com/vits56/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -4,9 +4,7 @@ import com.vits56.maratonajava.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Haikyuu", "TV", 25, "Shounen");
-        Anime anime2 = new Anime("Naruto", "TV", 220, "Shounen");
+        Anime anime = new Anime("Haikyuu", "TV", 25, "Shounen", "Production I.G");
         anime.imprime();
-        anime2.imprime();
     }
 }
