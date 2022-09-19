3988121e4353c6c0652054eb87eb5f5c1f0f00a4
vits56 <bastosduartebruno@gmail.com>
2022-09-19 11:28:38 -0300

    61 - Orientação Objetos - Modificador static

---
+++ src/com/vits56/maratonajava/javacore/Fmodificadoresestaticos/test/CarroTest01.java
@@ -0,0 +1,17 @@
+package com.vits56.maratonajava.javacore.Fmodificadoresestaticos.test;
+
+import com.vits56.maratonajava.javacore.Fmodificadoresestaticos.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro c1 = new Carro("BMW", 280);
+        Carro c2 = new Carro("Audi", 275);
+        Carro c3 = new Carro("Mercedes", 290);
+
+        Carro.velocidadeLimite = 180;
+        c1.imprime();
+
+        c2.imprime();
+        c3.imprime();
+    }
+}
