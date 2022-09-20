4ea008a206b674feb67f42a7520c3b67437b1734
vits56 <bastosduartebruno@gmail.com>
2022-09-20 11:28:45 -0300

    64 - Orientação Objetos - Associação pt 01 - Arrays com Objetos

---
+++ src/com/vits56/maratonajava/javacore/Gassociacao/test/JogadorTest01.java
@@ -0,0 +1,16 @@
+package com.vits56.maratonajava.javacore.Gassociacao.test;
+
+import com.vits56.maratonajava.javacore.Gassociacao.dominio.Jogador;
+
+public class JogadorTest01 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Pelé");
+        Jogador jogador2 = new Jogador("Ronaldo");
+        Jogador jogador3 = new Jogador("Messi");
+        Jogador[] jogadores = new Jogador[]{jogador1, jogador2, jogador3};
+
+        for (Jogador jogador : jogadores) {
+            jogador.imprime();
+        }
+    }
+}
