8f9ef7464c57d7496b2c1f6e79792c03312f00b1
vits56 <bastosduartebruno@gmail.com>
2022-09-20 16:10:31 -0300

    71 - Orientação Objetos - Herança pt 01

---
+++ src/com/vits56/maratonajava/javacore/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,28 @@
+package com.vits56.maratonajava.javacore.Hheranca.test;
+
+import com.vits56.maratonajava.javacore.Hheranca.dominio.Endereco;
+import com.vits56.maratonajava.javacore.Hheranca.dominio.Funcionario;
+import com.vits56.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 1");
+        endereco.setCep("00000-000");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("João");
+        pessoa.setCpf("000.000.000-00");
+        pessoa.setEndereco(endereco);
+
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Maria");
+        funcionario.setCpf("111.111.111-11");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(20000);
+
+        funcionario.imprime();
+
+    }
+}
