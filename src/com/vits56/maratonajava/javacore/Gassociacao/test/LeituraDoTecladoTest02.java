9491f07a1a6d4ea76f7da7dc1dae9c0ecc7f19fb
vits56 <bastosduartebruno@gmail.com>
2022-09-20 13:10:11 -0300

    69 - Orientação Objetos - Exercício leitura de dados do teclado

---
+++ src/com/vits56/maratonajava/javacore/Gassociacao/test/LeituraDoTecladoTest02.java
@@ -0,0 +1,20 @@
+package com.vits56.maratonajava.javacore.Gassociacao.test;
+
+import java.util.Scanner;
+
+public class LeituraDoTecladoTest02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("O Grande software de previsão do futuro");
+        System.out.println("Digite sua pergunta e eu responderei sim ou não");
+        String pergunta = input.nextLine();
+        if (pergunta.charAt(0) == ' ') {
+            System.out.println("Sim");
+        } else {
+            System.out.println("Não");
+        }
+
+
+    }
+}
