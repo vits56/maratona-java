94c768ee14facf9165391fbc35f8ac30ff2cb925
vits56 <bastosduartebruno@gmail.com>
2022-09-20 13:04:27 -0300

    68 - Orientação Objetos - Leitura de dados pelo console

---
+++ src/com/vits56/maratonajava/javacore/Gassociacao/test/LeituraDoTecladoTest01.java
@@ -0,0 +1,25 @@
+package com.vits56.maratonajava.javacore.Gassociacao.test;
+
+import java.util.Scanner;
+
+public class LeituraDoTecladoTest01 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = entrada.nextLine();
+
+        System.out.println("Digite sua idade: ");
+        int idade = entrada.nextInt();
+
+        System.out.println("Digite M ou F para seu sexo: ");
+        char sexo = entrada.next().charAt(0);
+
+        System.out.println("-----------------------------");
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Sexo: " + sexo);
+
+    }
+}
