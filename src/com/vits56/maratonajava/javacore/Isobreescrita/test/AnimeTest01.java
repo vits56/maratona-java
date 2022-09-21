30f584f40dcbbfec951eebd9886bb0e1d574e858
vits56 <bastosduartebruno@gmail.com>
2022-09-21 10:51:32 -0300

    76 - Orientação Objetos - Sobrescrita do método toString

---
+++ src/com/vits56/maratonajava/javacore/Isobreescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package com.vits56.maratonajava.javacore.Isobreescrita.test;
+
+import com.vits56.maratonajava.javacore.Isobreescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+
+        System.out.println(anime);
+    }
+}
