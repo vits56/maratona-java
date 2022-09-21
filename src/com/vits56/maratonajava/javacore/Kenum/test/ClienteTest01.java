e5aa30784a84ed070d1df89c802623884d1e9ad5
vits56 <bastosduartebruno@gmail.com>
2022-09-21 17:36:16 -0300

    81 - Orientação Objetos - Enumeração pt 02 - Construtores e atributos

--- src/com/vits56/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ src/com/vits56/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -1,19 +1,16 @@
 package com.vits56.maratonajava.javacore.Kenum.test;
 
 import com.vits56.maratonajava.javacore.Kenum.dominio.Cliente;
+import com.vits56.maratonajava.javacore.Kenum.dominio.Cliente.TipoPagamento;
 import com.vits56.maratonajava.javacore.Kenum.dominio.TipoCliente;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente3 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("Tsubasa", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Akira", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
 
     }
 }
