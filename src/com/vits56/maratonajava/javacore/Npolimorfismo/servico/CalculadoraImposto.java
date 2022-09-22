db906419a12369ff296809f4a21a5137aae391e9
vits56 <bastosduartebruno@gmail.com>
2022-09-22 18:01:56 -0300

    92 - Orientação Objetos - Polimorfismo pt 03 - Parâmetros polimórficos

--- src/com/vits56/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
+++ src/com/vits56/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
@@ -1,22 +1,15 @@
 package com.vits56.maratonajava.javacore.Npolimorfismo.servico;
 
 import com.vits56.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import com.vits56.maratonajava.javacore.Npolimorfismo.dominio.Produto;
 import com.vits56.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 
 public class CalculadoraImposto {
-    public static void calcularImposto(Computador computador) {
-        System.out.println("Relatório de imposto do Computador");
-        double imposto = computador.calcularImposto();
-        System.out.println("Computador: " + computador.getNome());
-        System.out.println("Valor " + computador.getValor());
-        System.out.println("Imposto a ser pago: " + imposto);
-    }
-
-    public static void calcularImpostoTomate(Tomate tomate) {
-        System.out.println("Relatório de imposto do tomate");
-        double imposto = tomate.calcularImposto();
-        System.out.println("Tomate: " + tomate.getNome());
-        System.out.println("Valor " + tomate.getValor());
+    public static void calcularImposto(Produto produto) {
+        System.out.println("Relatório de imposto do produto");
+        double imposto = produto.calcularImposto();
+        System.out.println("Produto: " + produto.getNome());
+        System.out.println("Valor " + produto.getValor());
         System.out.println("Imposto a ser pago: " + imposto);
     }
 
--- src/com/vits56/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
+++ src/com/vits56/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
@@ -1,6 +1,7 @@
 package com.vits56.maratonajava.javacore.Npolimorfismo.test;
 
 import com.vits56.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import com.vits56.maratonajava.javacore.Npolimorfismo.dominio.Televisao;
 import com.vits56.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 import com.vits56.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
 
@@ -8,9 +9,12 @@ public class ProdutoTest01 {
     public static void main(String[] args) {
         Computador computador = new Computador("Notebook", 2000);
         Tomate tomate = new Tomate("Tomate", 10);
+        Televisao televisao = new Televisao("Televisao", 1000);
 
         CalculadoraImposto.calcularImposto(computador);
         System.out.println("----------------------------");
-        CalculadoraImposto.calcularImpostoTomate(tomate);
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("----------------------------");
+        CalculadoraImposto.calcularImposto(televisao);
     }
 }
