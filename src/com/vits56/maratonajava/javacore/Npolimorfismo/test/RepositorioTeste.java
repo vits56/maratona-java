e516b0126489018844828623d470501b804299a4
vits56 <bastosduartebruno@gmail.com>
2022-09-22 22:39:33 -0300

    95 - Exceções pt 01 - Errors

--- src/com/vits56/maratonajava/javacore/Npolimorfismo/test/RepositorioTeste.java
+++ src/com/vits56/maratonajava/javacore/Npolimorfismo/test/RepositorioTeste.java
@@ -1,9 +1,7 @@
 package com.vits56.maratonajava.javacore.Npolimorfismo.test;
 
 import com.vits56.maratonajava.javacore.Npolimorfismo.repositorio.Repositorio;
-import com.vits56.maratonajava.javacore.Npolimorfismo.servico.RepositorioArquivo;
 import com.vits56.maratonajava.javacore.Npolimorfismo.servico.RepositorioBancoDeDados;
-import com.vits56.maratonajava.javacore.Npolimorfismo.servico.RepositorioMemoria;
 
 public class RepositorioTeste {
     public static void main(String[] args) {
