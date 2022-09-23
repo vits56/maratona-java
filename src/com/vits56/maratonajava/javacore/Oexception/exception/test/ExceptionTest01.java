65e3a9adbc2447275dc11bd45c22cc65b24dcf48
vits56 <bastosduartebruno@gmail.com>
2022-09-22 23:24:18 -0300

    97 - Exceções pt 03 - Exception

---
+++ src/com/vits56/maratonajava/javacore/Oexception/exception/test/ExceptionTest01.java
@@ -0,0 +1,21 @@
+package com.vits56.maratonajava.javacore.Oexception.exception.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest01 {
+    public static void main(String[] args) {
+        criarNovoArquivo();
+    }
+
+    private static void criarNovoArquivo() {
+        File file = new File("arquivo//teste.txt");
+        try {
+            boolean isCriado = file.createNewFile();
+            System.out.println("Arquivo criado " + isCriado);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
