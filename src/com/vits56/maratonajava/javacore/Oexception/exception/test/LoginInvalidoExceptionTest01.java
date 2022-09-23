6977b519711fba054519ed36449630e649c2fbaa
vits56 <bastosduartebruno@gmail.com>
2022-09-23 18:49:27 -0300

    104 - Exceções pt 10 - Exceção customizada

---
+++ src/com/vits56/maratonajava/javacore/Oexception/exception/test/LoginInvalidoExceptionTest01.java
@@ -0,0 +1,31 @@
+package com.vits56.maratonajava.javacore.Oexception.exception.test;
+
+import com.vits56.maratonajava.javacore.Oexception.exception.dominio.LoginInvalidoException;
+
+import java.util.Scanner;
+
+public class LoginInvalidoExceptionTest01 {
+    public static void main(String[] args) {
+        try {
+            logar();
+        } catch (LoginInvalidoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void logar() throws LoginInvalidoException {
+        Scanner teclado = new Scanner(System.in);
+        String usernameDB = "Goku";
+        String senhaDB = "ssj";
+        System.out.println("Digite o username");
+        String usernameDigitado = teclado.nextLine();
+        System.out.println("Digite a senha");
+        String senhaDigitada = teclado.nextLine();
+
+        if (!usernameDB.equals(usernameDigitado) || !senhaDB.equals(senhaDigitada)) {
+            throw new LoginInvalidoException("Usuário ou senha inválidos");
+        }
+
+        System.out.println("Logado com sucesso");
+    }
+}
