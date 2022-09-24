88d8adf299d0edb00563cdc2787856da4e5ef6be
vits56 <bastosduartebruno@gmail.com>
2022-09-24 12:49:14 -0300

    107 - Classes Utilitárias - Wrappers pt 02

--- src/com/vits56/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
+++ src/com/vits56/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -13,11 +13,26 @@ public class WrapperTest01 {
 
         Byte byteW = 1;
         Short shortW = 1;
-        Integer intW = 1;
+        Integer intW = 1;// autoboxing
         Long longW = 10L;
         Float floatW = 10F;
         Double doubleW = 100D;
         Character charW = 'A';
         Boolean booleanW = true;
+
+        int i = intW; // unboxing
+        Integer intW2 = Integer.parseInt("1");
+        System.out.println(intW2);
+        System.out.println("------------------");
+        boolean verdadeiro = Boolean.parseBoolean("TrUe");
+        System.out.println(verdadeiro);
+        System.out.println("------------------");
+        System.out.println(Character.isDigit('9'));
+        System.out.println(Character.isLetter('A'));
+        System.out.println(Character.isLetterOrDigit('!'));
+        System.out.println(Character.isUpperCase('A'));
+        System.out.println(Character.isLowerCase('a'));
+        System.out.println(Character.toUpperCase('a'));
+        System.out.println(Character.toLowerCase('A'));
     }
 }
