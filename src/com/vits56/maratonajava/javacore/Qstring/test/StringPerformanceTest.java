78ad97d71f4af61dd240212260374a364520f8cf
vits56 <bastosduartebruno@gmail.com>
2022-09-24 16:18:58 -0300

    110 - Classes Utilitárias - Strings pt 03 - Desempenho

---
+++ src/com/vits56/maratonajava/javacore/Qstring/test/StringPerformanceTest.java
@@ -0,0 +1,45 @@
+package com.vits56.maratonajava.javacore.Qstring.test;
+
+public class StringPerformanceTest {
+    public static void main(String[] args) {
+        Long inicio = System.currentTimeMillis();
+        concatString(50_000);
+        Long fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para String: " + (fim - inicio) + "ms");
+
+        System.out.println("------------------");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuilder(500_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para StringBuilder: " + (fim - inicio) + "ms");
+
+        System.out.println("------------------");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuffer(500_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para StringBuffer: " + (fim - inicio) + "ms");
+    }
+
+    public static void concatString(int tamanho) {
+        String texto = "";
+        for (int i = 0; i < tamanho; i++) {
+            texto += i;
+        }
+    }
+
+    public static void concatStringBuilder(int tamanho) {
+        StringBuilder sb = new StringBuilder(tamanho);
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
+        }
+    }
+
+    public static void concatStringBuffer(int tamanho) {
+        StringBuffer sb = new StringBuffer(tamanho);
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
+        }
+    }
+}
