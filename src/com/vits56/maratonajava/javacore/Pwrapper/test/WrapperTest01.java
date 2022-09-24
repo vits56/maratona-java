b9fae39ec252609bc431c6093098e743112cc933
vits56 <bastosduartebruno@gmail.com>
2022-09-24 12:33:47 -0300

    106 - Classes Utilitárias - Wrappers pt 01

---
+++ src/com/vits56/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -0,0 +1,23 @@
+package com.vits56.maratonajava.javacore.Pwrapper.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 100;
+        char charP = 'A';
+        boolean booleanP = true;
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 100D;
+        Character charW = 'A';
+        Boolean booleanW = true;
+    }
+}
