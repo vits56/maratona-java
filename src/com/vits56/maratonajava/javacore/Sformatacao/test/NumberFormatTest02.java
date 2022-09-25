afc96237a3da9f9f15b05626b4e66f251161a2b2
vits56 <bastosduartebruno@gmail.com>
2022-09-25 17:05:51 -0300

    117 - Classes Utilitárias - Internacionalização de moeda com Locale

--- src/com/vits56/maratonajava/javacore/Sformatacao/test/NumberFormatTest02.java
+++ src/com/vits56/maratonajava/javacore/Sformatacao/test/NumberFormatTest02.java
@@ -3,22 +3,22 @@ package com.vits56.maratonajava.javacore.Sformatacao.test;
 import java.text.NumberFormat;
 import java.util.Locale;
 
-public class NumberFormatTest01 {
+public class NumberFormatTest02 {
     public static void main(String[] args) {
         Locale localePtBr = new Locale("pt", "BR");
         Locale localeJP = Locale.JAPAN;
         Locale localeItaly = Locale.ITALY;
         NumberFormat[] nfa = new NumberFormat[4];
 
-        nfa[0] = NumberFormat.getInstance();
-        nfa[1] = NumberFormat.getInstance(localeJP);
-        nfa[2] = NumberFormat.getInstance(localePtBr);
-        nfa[3] = NumberFormat.getInstance(localeItaly);
+        nfa[0] = NumberFormat.getCurrencyInstance();
+        nfa[1] = NumberFormat.getCurrencyInstance(localeJP);
+        nfa[2] = NumberFormat.getCurrencyInstance(localePtBr);
+        nfa[3] = NumberFormat.getCurrencyInstance(localeItaly);
 
         double valor = 10_000.2130;
 
         for (NumberFormat numberFormat : nfa) {
-            System.out.println(numberFormat.getMaximumFractionDigits());
+            numberFormat.setMaximumFractionDigits(2);
             System.out.println(numberFormat.format(valor));
         }
 
