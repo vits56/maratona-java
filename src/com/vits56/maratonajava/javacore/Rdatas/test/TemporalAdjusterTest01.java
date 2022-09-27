f3167678b357bb1d22d9c32e4657ff429a4b7e9b
vits56 <bastosduartebruno@gmail.com>
2022-09-26 23:51:06 -0300

    127 - Classes Utilitárias - TemporalAdjuster

--- src/com/vits56/maratonajava/javacore/Rdatas/test/TemporalAdjusterTest01.java
+++ src/com/vits56/maratonajava/javacore/Rdatas/test/TemporalAdjusterTest01.java
@@ -3,22 +3,43 @@ package com.vits56.maratonajava.javacore.Rdatas.test;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.ChronoField;
-import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+
+class obterProximoDiaUtil implements TemporalAdjuster {
+
+    public Temporal adjustInto(Temporal temporal) {
+        DayOfWeek dayOfWeek = DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK));
+        int addDays;
+        switch (dayOfWeek) {
+            case THURSDAY:
+                addDays = 4;
+                break;
+            case FRIDAY:
+                addDays = 3;
+                break;
+            case SATURDAY:
+                addDays = 2;
+                break;
+            default:
+                addDays = 1;
+        }
+        return temporal.plus(addDays, ChronoUnit.DAYS);
+    }
+}
 
 public class TemporalAdjusterTest01 {
     public static void main(String[] args) {
         LocalDate now = LocalDate.now();
-        now = now.with(ChronoField.DAY_OF_MONTH, 26);
         System.out.println(now);
         System.out.println(now.getDayOfWeek());
 
-        now = LocalDate.now().with(TemporalAdjusters.nextOrSame(DayOfWeek.MONDAY));
+        now = LocalDate.now().with(new obterProximoDiaUtil());
         System.out.println(now);
         System.out.println(now.getDayOfWeek());
 
-        now = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY));
-        System.out.println(now);
-        System.out.println(now.getDayOfWeek());
+
 
     }
 }
---
+++ src/com/vits56/maratonajava/javacore/Rdatas/test/TemporalAdjustersTest01.java
@@ -0,0 +1,24 @@
+package com.vits56.maratonajava.javacore.Rdatas.test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalAdjusters;
+
+public class TemporalAdjustersTest01 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        now = now.with(ChronoField.DAY_OF_MONTH, 26);
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.nextOrSame(DayOfWeek.MONDAY));
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+    }
+}
